b3156157f8e5e9fbddac9c5b250f97f9e0d7e5f0
Mark Lee <electronjs@lazymalevolence.com>
2019-06-25 18:13:49 -0700

    chore: replace tslint with prettier

--- src/spawn.ts
+++ src/spawn.ts
@@ -9,15 +9,26 @@ export interface SpawnResult {
   output: string;
 }
 
-export const spawn = (cmd: string, args: string[] = [], opts: SpawnOptions = {}): Promise<SpawnResult> => {
-  d('spawning cmd:', cmd, 'args:', args.map(arg => isSecret(arg) ? '*********' : arg), 'opts:', opts);
+export const spawn = (
+  cmd: string,
+  args: string[] = [],
+  opts: SpawnOptions = {},
+): Promise<SpawnResult> => {
+  d(
+    'spawning cmd:',
+    cmd,
+    'args:',
+    args.map(arg => (isSecret(arg) ? '*********' : arg)),
+    'opts:',
+    opts,
+  );
   const child = cpSpawn(cmd, args, opts);
   const out: string[] = [];
   const dataHandler = (data: Buffer) => out.push(data.toString());
   child.stdout.on('data', dataHandler);
   child.stderr.on('data', dataHandler);
-  return new Promise<SpawnResult>((resolve) => {
-    child.on('exit', (code) => {
+  return new Promise<SpawnResult>(resolve => {
+    child.on('exit', code => {
       d(`cmd ${cmd} terminated with code: ${code}`);
       resolve({
         code,
